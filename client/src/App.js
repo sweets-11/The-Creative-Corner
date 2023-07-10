c90db823fb6f75297488ed84eae984c395e71156
Aniket Jain <aniketjain2018@gmail.com>
2023-07-11 03:22:16 +0530

    created footer and also define routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import {ThemeProvider} from "@mui/material/styles"
-import theme from "./theme";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./global/Navbar";
+import Home from "./global/Home";
+import Footer from "./global/Footer";
+
+import {
+  ContactUs,
+  NewsLetter,
+  SavedBlog,
+} from "./components/index";
+
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <div>App</div>
-    </ThemeProvider>
-  )
-}
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/savedBlog" element={<SavedBlog />} />
+        <Route path="/newsLetter" element={<NewsLetter />} />
+        <Route path="/contactUs" element={<ContactUs />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Blog} from "./Blog";
+export {default as Carousel} from "./Carousel";
+export {default as ContactUs} from "./ContactUs";
+export {default as NewsLetter} from "./NewsLetter";
+export {default as SavedBlog} from "./SavedBlog";
