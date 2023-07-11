9c31d2781e4476aa9796bb8ae362297c83a0bd67
Aniket Jain <aniketjain2018@gmail.com>
2023-07-11 20:59:01 +0530

    created contactup component

--- client/src/components/ContactUs.jsx
+++ client/src/components/ContactUs.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Box,
+} from "@mui/material";
+import { css } from "@emotion/react";
+
+const styles = {
+  container: css`
+    padding: 20px;
+  `,
+  title: css`
+    text-align: center;
+  `,
+};
 
 const ContactUs = () => {
-  return (
-    <div>ContactUs</div>
-  )
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your logic here to handle form submission
+  };
 
-export default ContactUs
+  return (
+    <Container css={styles.container}>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Typography variant="h2" margin="30px 0 20px" css={styles.title}>
+          Contact Us
+        </Typography>
+      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField label="Name" variant="outlined" required fullWidth />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Email"
+            variant="outlined"
+            required
+            type="email"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="Message"
+            variant="outlined"
+            required
+            multiline
+            rows={4}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default ContactUs;
